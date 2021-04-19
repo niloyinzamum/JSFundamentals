a38e0102b3d86710b10a521574d7eba40c98c4fd
name <inzi141@gmail.com>
2021-04-19 19:18:41 +0600

    done

--- promises.js
+++ promises.js
@@ -1,22 +1,40 @@
-const posts = [
-    {title: 'Post one', body:'This is post one'},
-    {title: 'Post two', body:'This is post two'}
-];
+// const posts = [
+//     {title: 'Post one', body:'This is post one'},
+//     {title: 'Post two', body:'This is post two'}
+// ];
+//
+// function getPosts(){
+//     setTimeout(()=>{
+//         let output = '';
+//         posts.forEach((post, index) =>{
+//             output += `<li>${post.title}</li>`;
+//         });
+//         console.log(output);
+//
+//     }, 1000);
+// }
+//
+// function createPosts(post, callback){
+//     setTimeout(()=>{
+//         posts.push(post);
+//         callback();
+//     }, 2000)
+// }
 
-function getPosts(){
-    setTimeout(()=>{
-        let output = '';
-        posts.forEach((post, index) =>{
-            output += `<li>${post.title}</li>`;
-        });
-        document.body.innerHTML = output;
-        
-    }, 1000);
-}
 
-function createPosts(post, callback){
-    setTimeout(()=>{
-        posts.push(post);
-        callback();
-    }, 2000)
-}
+let p = new Promise((resolve, reject)=>{
+    let a = 1+3
+    if (a==2)
+    {
+        resolve('success')
+    }
+    else {
+        reject("reject")
+    }
+})
+
+p.then((message)=>{
+    console.log('in then'+ message)
+}).catch((message)=>{
+    console.log('in catch'+ message)
+})
