c8f7caa37604d0fd763dbbbc30ee2c0ebafa4282
inzamum <niloy.inzamum@gmail.com>
2021-11-25 03:59:31 +0600

    git practice completed

---
+++ objects.js
@@ -0,0 +1,34 @@
+testObject = {
+    theValue : "world",
+    firstKey: "first value",
+    secondKey: "second value",
+    aFunc :function aFobjectFunction1(){
+        let theValue = "hello"
+        console.log(this.theValue+ ` ${theValue}`)
+    },
+    aFobjectFunction :function (){
+        let theValue = "hello"
+        console.log(theValue)
+    },
+    secondFunction(){
+        console.log("second function")
+    }
+}
+
+// console.log(testObject.firstKey)
+testObject.aFunc()
+// console.log(testObject.aFobjectFunction())
+// console.log(testObject.secondFunction())
+console.log(testObject)
+
+for(let i in testObject){
+    console.log(i)
+    console.log(testObject[i])
+}
+
+testObject.firstKey = "new value"
+console.log(testObject.firstKey)
+
+testObject["third_key"] = "value for third key"
+
+console.log(testObject.third_key)
