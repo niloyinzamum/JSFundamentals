603fe2d85abbc4b646101b319ca2c8739be518d4
name <inzi141@gmail.com>
2021-11-30 00:41:19 +0600

    class in progress

---
+++ classes.js
@@ -0,0 +1,4 @@
+//literal meaning - a set or category of things having some property or attribute in common and differentiated from others by kind, type, or quality.
+//In object-oriented programming, a class is an extensible program-code-template for creating objects, providing initial values for state and implementations of behavior.
+
+in progress
