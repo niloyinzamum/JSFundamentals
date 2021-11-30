79cab13442d02c9422dbc730b5cf4c2a7ba194c3
name <inzi141@gmail.com>
2021-11-30 20:56:07 +0600

    class in progress

--- classes.js
+++ classes.js
@@ -1,4 +1,15 @@
 //literal meaning - a set or category of things having some property or attribute in common and differentiated from others by kind, type, or quality.
 //In object-oriented programming, a class is an extensible program-code-template for creating objects, providing initial values for state and implementations of behavior.
 
-in progress
+class theClass {
+    constructor(){
+        console.log("this will run when the object is created")
+    }
+
+    foo() {
+        console.log("this will run when the object when object calls this")
+    }
+}
+
+const bar = new theClass()
+bar.foo()
