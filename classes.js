9860926dcf2ff8d7c8d5386ce4ecfd88f322833a
inzamum <niloy.inzamum@gmail.com>
2021-12-04 19:55:20 +0600

    classes in progress

--- classes.js
+++ classes.js
@@ -5,7 +5,6 @@ class theClass {
     constructor(){
         console.log("this will run when the object is created")
     }
-
     foo() {
         console.log("this will run when the object when object calls this")
     }
@@ -27,12 +26,12 @@ class MasterCLass{
         console.log(this.objVal) 
         console.log(takesValue)
     }
-
-
     }
    
  class ChildClass extends MasterCLass{
-
+    aNewFunc(){
+        this.anotherFunc(56)
+    }
  }   
 
 const masterObj =new MasterCLass(25)
@@ -40,3 +39,59 @@ masterObj.anotherFunc("this")
 
 const ChildClassObj = new ChildClass("childObject")
 ChildClassObj.anotherFunc(5)
+ChildClassObj.aNewFunc()
+
+class ANewMasterClass{
+    aVariable=10;
+    constructor(aVariable){
+        this.aVariable = aVariable
+    }
+    aMethod(){
+        console.log("this method says "+this.aVariable)
+    }
+}
+
+class ANewChildClass extends ANewMasterClass{
+    
+    constructor(aNewVariable){
+        super(aNewVariable)
+    }
+    childMethod(){
+        console.log("in child method: "+this.aVariable)
+        console.log(this.aVariable)
+        this.aMethod()
+    }
+}
+
+const childObj = new ANewChildClass(21)
+childObj.childMethod()
+// ------------------------------------------------------------------------------
+class Animal{
+    name;
+    constructor(name){
+        this.name = name
+    }
+
+    run(speed){
+        this.speed= speed
+        console.log("speed: "+ speed)
+    }
+
+    stop(){
+        this.speed =0
+        console.log("speed: "+ this.speed)
+        } 
+}
+
+class Dog extends Animal{
+    constructor(name){
+        super(name)
+    }
+    hide(){
+        console.log(this.name+ " hides")
+    }
+}
+const dogObj = new Dog("dog")
+dogObj.run(10)
+dogObj.stop()
+dogObj.hide()
