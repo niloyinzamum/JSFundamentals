fefd2bfffa366919cf22e89ceac7b6a07768d1a7
name <inzi141@gmail.com>
2021-12-25 14:39:04 +0600

    callbacks

--- JSbasics/callbacks.js
+++ JSbasics/callbacks.js
@@ -1,8 +1,33 @@
-function aFunc(param, callback){
-    const something = "something"
-    callback(param, something)
+//A callback function is a function passed into another function as an argument, 
+//which is then invoked inside the outer function to complete some kind of routine or action.
+function greeting(name){
+    console.log('hello '+ name);
 }
 
-aFunc("a string", (param, something)=>{
-    console.log(param, something)
-})
+function ProcessUserInput(callback) {
+    var name = "birdie"
+
+    //---------but first processUserInput function will run first-------------
+    console.log("---------but first processUserInput function will run first--------------")
+
+    //2. greeting function is taking the name as argument here and getting 
+    //revoked inside this function  
+    callback(name)
+}
+
+//1. sending the greeting function through this argument 
+ProcessUserInput(greeting)
+
+
+//--------------------------------another 
+function foo(value, vari){
+    console.log(`doing something with this ${vari}`)
+    console.log(value)
+}
+
+function bar(value, callback){
+    let vari = "variable"
+    callback(value, vari)
+}
+
+bar("another value", foo)
