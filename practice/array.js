000b6f1b8ec3483347e2367a8b34364926af0804
inzamum <niloy.inzamum@gmail.com>
2022-01-11 00:03:22 +0600

    array practice

--- practice/array.js
+++ practice/array.js
@@ -10,12 +10,26 @@ let anArray = ["first", "word", 1, 2, true, false, {
 }]
 
 //regular for loop
-for(i=0; i<anArray.length; i++){
-    console.log(anArray[i])
-}
+// for(i=0; i<anArray.length; i++){
+//     console.log(anArray[i])
+// }
 
 //for in loop
-for(index in anArray)
-{
-    console.log(anArray[index])
+// for(index in anArray)
+// {
+//     console.log(anArray[index])
+// }
+
+//for each loop (calls a function for each element in an array)
+// anArray.forEach(theFunc)
+
+// function theFunc(element, index, theArrray){
+//     console.log("element: "+element)
+//     console.log("index: "+index)
+//     console.log("theArray:"+theArrray)
+// }
+
+//for of loop
+for (elements of anArray){
+    console.log(elements)
 }
